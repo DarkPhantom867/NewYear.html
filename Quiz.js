72815465c06d93c37f969853a540d06919ccfb6b
DarkPhantom867 <94231108+DarkPhantom867@users.noreply.github.com>
2022-01-04 20:33:28 +0400

    Add files via upload

---
+++ Quiz.js
@@ -0,0 +1,18 @@
+function ans1() {
+    document.getElementById("text1").innerHTML="Answer: Straw Dolls";}
+
+function ans2() {
+document.getElementById("text2").innerHTML="Answer: Holly Night";   
+}
+
+function ans3() {
+    document.getElementById("text3").innerHTML="Answer: Germany";   
+    }
+
+function ans4() {
+    document.getElementById("text4").innerHTML="Answer: Feliz Navidad";   
+    }
+
+function ans5() {
+    document.getElementById("text5").innerHTML="Answer: Rudolph";   
+    }
